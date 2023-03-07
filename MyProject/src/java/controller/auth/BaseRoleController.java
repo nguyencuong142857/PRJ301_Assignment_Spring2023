6c8c0d2a7256c53dff19386944a19e3a63e21c16
nguyencuong142857 <111842969+nguyencuong142857@users.noreply.github.com>
2023-03-07 22:58:55 +0700

    create /login

---
+++ MyProject/src/java/controller/auth/BaseRoleController.java
@@ -0,0 +1,65 @@
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package controller.auth;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import model.Account;
+import model.Feature;
+import model.Role;
+
+/**
+ *
+ * @author sonnt
+ */
+public abstract class BaseRoleController extends BaseAuthenticationController {
+
+    private boolean isAuthorized(HttpServletRequest req)
+    {
+       Account account = (Account)req.getSession().getAttribute("account");
+       String currentUrl = req.getServletPath();//"/student/add"
+        for (Role role : account.getRoles()) {
+            for (Feature feature : role.getFeatures()) {
+                if(feature.getFeatureUrl().equals(currentUrl))
+                    return true;
+            }
+        }
+        return false;
+    }
+    
+    @Override
+    protected void processPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(isAuthorized(req))
+        {
+            processPost(req, resp, (Account)req.getSession().getAttribute("account"));
+        }
+        else
+        {
+            req.getSession().setAttribute("account", null);
+            req.getSession().setAttribute("errmsg", "You don't have permission to access this page");
+            resp.sendRedirect("../login");
+        }
+    }
+    
+    protected abstract void processPost(HttpServletRequest req, HttpServletResponse resp,Account account) throws ServletException, IOException;
+    protected abstract void processGet(HttpServletRequest req, HttpServletResponse resp,Account account) throws ServletException, IOException;
+    @Override
+    protected void processGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if(isAuthorized(req))
+        {
+            processGet(req, resp, (Account)req.getSession().getAttribute("account"));
+        }
+        else
+        {
+            req.getSession().setAttribute("account", null);
+            req.getSession().setAttribute("errmsg", "You don't have permission to access this page");
+            resp.sendRedirect("../login");
+        }
+    }
+    
+}
