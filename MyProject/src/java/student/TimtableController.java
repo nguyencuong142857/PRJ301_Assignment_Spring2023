ddfc3818adb891f8708daa2ac54d79cdcf5e12b9
NguyenCuong <111842969+nguyencuong142857@users.noreply.github.com>
2023-03-17 12:06:37 +0700

    Create TimtableController.java

---
+++ MyProject/src/java/student/TimtableController.java
@@ -0,0 +1,95 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package student;
+
+import controller.auth.BaseRoleController;
+import dao.AttendanceDBContext;
+import dao.SessionDBContext;
+import dao.StudentDBContext;
+import dao.TimeSlotDBContext;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.Date;
+import model.Account;
+import model.Attandance;
+import model.Session;
+import model.TimeSlot;
+import util.DateTimeHelper;
+
+/**
+ *
+ * @author admin
+ */
+public class TimtableController extends BaseRoleController {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response, Account account)
+            throws ServletException, IOException {
+        StudentDBContext stdb = new StudentDBContext();
+        int stdid = stdb.getStudent(account.getUsername()).getId();
+        String raw_from = request.getParameter("from");
+        String raw_to = request.getParameter("to");
+        java.sql.Date from = null;
+        java.sql.Date to = null;
+        if(raw_from ==null || raw_from.length() ==0)
+        {
+            Date today = new Date();
+            int todayOfWeek = DateTimeHelper.getDayofWeek(today);
+            Date e_from = DateTimeHelper.addDays(today, 2 - todayOfWeek);
+            Date e_to = DateTimeHelper.addDays(today, 8-todayOfWeek);
+            from = DateTimeHelper.toDateSql(e_from);
+            to = DateTimeHelper.toDateSql(e_to);
+        }
+        else
+        {
+            from = java.sql.Date.valueOf(raw_from);
+            to = java.sql.Date.valueOf(raw_to);
+        }
+        
+        
+        request.setAttribute("username", account.getUsername());
+        request.setAttribute("from", from);
+        request.setAttribute("to", to);
+        request.setAttribute("dates", DateTimeHelper.getDateList(from, to));
+        request.setAttribute("days", DateTimeHelper.getDayMonthList(from, to));
+        
+        TimeSlotDBContext slotDB = new TimeSlotDBContext();
+        ArrayList<TimeSlot> slots = slotDB.list();
+        request.setAttribute("slots", slots);
+        
+        AttendanceDBContext attdb = new AttendanceDBContext();
+        ArrayList<Attandance> atts = attdb.filterStudent(stdid, from, to);
+
+        request.setAttribute("atts", atts);
+        
+        
+        request.getRequestDispatcher("../view/student/timetable.jsp").forward(request, response); 
+    }
+
+    
+    @Override
+    protected void processPost(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        processRequest(req, resp, account);
+    }
+
+    @Override
+    protected void processGet(HttpServletRequest req, HttpServletResponse resp, Account account) throws ServletException, IOException {
+        processRequest(req, resp, account);
+    }
+
+}
